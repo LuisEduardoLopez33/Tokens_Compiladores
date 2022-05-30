83424dd17fdd8577662ff5292e8974563438d2ef
LuisEduardoLopez33 <193274@ids.upchiapas.edu.mx>
2022-05-29 22:32:09 -0500

    expresiones regulares basicas

--- src/tokens_javaToSql/Model/expresionesRegulares.java
+++ src/tokens_javaToSql/Model/expresionesRegulares.java
@@ -1,4 +1,20 @@
 package tokens_javaToSql.Model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class expresionesRegulares {
+    public boolean letras(String dato){
+        Pattern pat = Pattern.compile("[A-Za-z]+");
+        Matcher mat = pat.matcher(dato);
+
+        return mat.matches();
+    }
+
+    public boolean validarNumeros(String dato){
+        Pattern pat = Pattern.compile("[0-9]+");
+        Matcher mat = pat.matcher(dato);
+
+        return mat.matches();
+    }
 }
