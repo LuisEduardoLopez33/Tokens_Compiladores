6f7b55175fe666cd675d5ec4224a4dda16bb6562
Uziel <201274@ids.upchiapas.edu.mx>
2022-05-29 22:18:45 -0500

    creacion del boton y text area, separacion de palabras

--- src/tokens_javaToSql/Controller/ControllerInicio.java
+++ src/tokens_javaToSql/Controller/ControllerInicio.java
@@ -1,4 +1,32 @@
 package tokens_javaToSql.Controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+
 public class ControllerInicio {
+
+    ObservableList<String> entrad = FXCollections.observableArrayList();
+
+    @FXML
+    private TextArea entrada;
+
+    @FXML
+    private Label salida;
+
+    @FXML
+    void iniciar(MouseEvent event) {
+        String[] sa = entrada.getText().split(" ");
+
+        //guardando al array
+        for (int i = sa.length-1; i>=0; i--){
+            entrad.add(sa[i]);
+        }
+
+        System.out.println(entrad);
+    }
+
 }
